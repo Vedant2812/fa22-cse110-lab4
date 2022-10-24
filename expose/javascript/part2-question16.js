a58237d7492feb823812512e0cc4e666428015bb
Vedant2812 <114892387+Vedant2812@users.noreply.github.com>
2022-10-23 20:32:13 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,3 +1,12 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2
+};
+
 for(const prop in statistics) {
   if((statistics[prop] % 2 != 0) || prop.startsWith("r")) {
     console.log(statistics[prop]);
